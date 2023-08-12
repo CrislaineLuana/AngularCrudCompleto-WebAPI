c613372037841b2c2b1e5e011ac2738bb8367e8c
Crislaine Luana <clpaula3007@gmail.com>
2023-08-12 08:17:22 -0300

    RetirandoHeadereFooter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { DetalhesComponent } from './pages/detalhes/detalhes.component';
@@ -28,8 +26,6 @@ import { ExcluirComponent } from './components/excluir/excluir.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
     CadastroComponent,
     DetalhesComponent,
